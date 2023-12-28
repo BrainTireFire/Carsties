2b578f927c7cfda22c44454c19627f01c5d0af25
Maciej Kawęcki <skimiskim12@gmail.com>
2023-12-28 15:58:28 +0100

    Add nextAuth to the client

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Auction, PagedResult } from "@/types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   const res = await fetch(`http://localhost:6001/search${query}`);
@@ -11,4 +12,30 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
 
   const data = await res.json();
   return data;
+}
+
+export async function UpdateAuctionTest() {
+  const data = {
+    mileage: Math.floor(Math.random() * 100000) + 1,
+  };
+
+  const token = await getTokenWorkaround();
+
+  const res = await fetch(
+    "http://localhost:6001/auctions/c8c3ec17-01bf-49db-82aa-1ef80b833a9f",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token?.access_token,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) {
+    return { status: res.status, message: res.statusText };
+  }
+
+  return res.statusText;
 }
--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { useParamsStore } from "@/hooks/useParamsStore";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { use } from "react";
 import { AiOutlineCar } from "react-icons/ai";
 
 export default function Logo() {
+  const router = useRouter();
+  const pathname = usePathname();
   const reset = useParamsStore((state) => state.reset);
+
+  function doReset() {
+    if (pathname !== "/") {
+      router.push("/");
+    }
+    reset();
+  }
 
   return (
     <div
-      onClick={reset}
+      onClick={doReset}
       className="flex items-center gap-2 text-3xl font-semibold text-red-500 
         cursor-pointer"
     >
