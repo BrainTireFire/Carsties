f76774f004acc139f320cabd611a5cb5abaa442a
Maciej Kawęcki <skimiskim12@gmail.com>
2023-12-30 11:13:02 +0100

    Add the CRUD operations to client

--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -3,8 +3,13 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: string;
       username: string;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    username: string;
   }
 
   interface Profile {
