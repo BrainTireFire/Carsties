0d38bcafe920e79e3ecd773666f7c81d80f279fd
Maciej Kawęcki <skimiskim12@gmail.com>
2023-12-31 11:14:37 +0100

    Add notification service SignalR

---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using AuctionService;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService;
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> bid placed message received");
+
+        await _hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+    }
+}
